b36d5cd4a0bfb0963476a0e8c0571b874b4b1608
pavelgonchar <pavel.gonchar@gmail.com>
2018-05-20 06:22:12 -0700

    remove indoors from index

--- create_annoy_index.py
+++ create_annoy_index.py
@@ -7,12 +7,25 @@ ANNOY_INDEX = AnnoyIndex(FEATURES_NUMBER)
 
 lfh = open("labels.ann", "w")
 
+INDOOR_IMAGES_URL = 'https://s3-us-west-2.amazonaws.com/kaggleglm/train_indoor.txt'
+INDOOR_IMAGES_PATH = get_file(
+    'train_indoor.txt',
+    INDOOR_IMAGES_URL,
+    cache_subdir='models',
+    file_hash='a0ddcbc7d0467ff48bf38000db97368e')
+indoor_images = set(open(INDOOR_IMAGES_PATH, 'r').read().splitlines())
+
+indoors = set(open("train_indoor.txt").read().splitlines())
 files = glob.glob("features/AXception-cs192/*.npy")
 i = 0
 for file_name in files:
     vectors = np.load(file_name)
-    if len(file_name.split('/')[-1].split('.')[0]) == 16:
+    train_id = file_name.split('/')[-1].split('.')[0]
+    if len(train_id) == 16:
         continue
+    if train_id in indoor_images:
+        continue
+
     label = int(file_name.split('/')[-1].split('.')[0])
     for j in range(len(vectors)):
         lfh.write("%s %s\n" % (i, label))
