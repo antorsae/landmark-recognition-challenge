ee48f67ac45914c27804b2eb3eaa8fa4c6813950
pavelgonchar <pavel.gonchar@gmail.com>
2018-05-21 14:04:16 -0700

    class lookup ext

--- class_lookup.py
+++ class_lookup.py
@@ -6,12 +6,19 @@ import cv2
 import numpy as np
 label = sys.argv[1]
 
+test_id = None
+if ',' in label:
+    test_id, label = label.split(',')
+
 _, output = commands.getstatusoutput(
     "grep -E ',%s$' ../../../train.csv | cut -f1 -d','" % label)
 
 
-def read_image(image_id):
-    img_path = '../../train-dl/%s.jpg' % image_id
+def read_image(image_id, test):
+    if test == 1:
+        img_path = '../../test-dl/%s.jpg' % image_id
+    else:
+        img_path = '../../train-dl/%s.jpg' % image_id
     try:
         img = jpeg.JPEG(img_path).decode()
     except:
@@ -22,7 +29,7 @@ def read_image(image_id):
 
 i = 0
 for image_id in output.split('\n'):
-    img = read_image(image_id)
+    img = read_image(image_id, 0)
     if len(img.shape) != 3:
         continue
     img = img[:, :, ::-1]
@@ -38,3 +45,15 @@ _, output = commands.getstatusoutput(
     'aws s3 cp /tmp/%s.jpg s3://kaggleglm/class_lookup/ --grants full=uri=http://acs.amazonaws.com/groups/global/AllUsers'
     % label)
 print 'https://s3-us-west-2.amazonaws.com/kaggleglm/class_lookup/%s.jpg' % label
+
+if test_id is not None:
+    img = read_image(test_id, 1)
+    img = img[:, :, ::-1]
+    cv2.imwrite('/tmp/%s.jpg' % test_id, img)
+
+    commands.getstatusoutput('montage -label %t ' + '-geometry +2+2  /tmp/%s.jpg /tmp/%s.jpg /tmp/%s_%s.jpg' % (test_id, label, test_id, label))
+
+    _, output = commands.getstatusoutput(
+        'aws s3 cp /tmp/%s_%s.jpg s3://kaggleglm/class_lookup/ --grants full=uri=http://acs.amazonaws.com/groups/global/AllUsers'
+        % (test_id, label))
+    print 'https://s3-us-west-2.amazonaws.com/kaggleglm/class_lookup/%s_%s.jpg' % (test_id, label)
